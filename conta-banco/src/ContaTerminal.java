f3785aa02b51f6362e47c6e3f4ff7fceeb01a1f6
Patricia Ducci Rizzo <pddrizzo@gmail.com>
2023-09-03 18:33:33 -0300

    Primeiro commit do projeto conta-banco

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; //para receber input de dados
+import java.util.Locale; //para ativar o ponto decimal do valor
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+      
+        System.out.println("** Bem vindo ao banco TESTE **");
+        System.out.println("Por favor, digite seu nome ");
+        String nomeCliente = scanner.next(); //entende apenas uma palavra no nome -> entender melhor
+        System.out.println("Informe sua agência ");
+        String agencia = scanner.next();
+        System.out.println("Informe sua conta ");
+        int conta = scanner.nextInt();
+        System.out.println("Informe seu saldo ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+        System.out.println("Olá " + nomeCliente + ", obrigada por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo de " + saldo + " já está disponível para saque.");
+
+    }
+}
